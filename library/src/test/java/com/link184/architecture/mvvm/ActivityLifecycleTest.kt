38a361ee7eb0334f4fe66be823e1d95d9d199d7e
Link184 <pacific.trac@gmail.com>
2020-02-27 06:03:15 +0200

    Test coverage of activity lifecycle

--- library/src/test/java/com/link184/architecture/mvvm/ActivityLifecycleTest.kt
+++ library/src/test/java/com/link184/architecture/mvvm/ActivityLifecycleTest.kt
@@ -3,6 +3,11 @@ package com.link184.architecture.mvvm
 import android.os.Build
 import com.link184.architecture.mvvm.base.MvvmActivity
 import com.link184.architecture.mvvm.component.TestActivity
+import com.nhaarman.mockitokotlin2.atLeastOnce
+import com.nhaarman.mockitokotlin2.reset
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -16,8 +21,48 @@ class ActivityLifecycleTest {
     private val testActivity: MvvmActivity<*>
         get() = testActivityController.get()
 
-    @Test
-    fun `test activity`() {
+    @Before
+    fun setUp() {
+        testActivityController.create()
+    }
 
+    @After
+    fun tearDown() {
+        reset(testActivity.viewModel)
+    }
+
+    @Test
+    fun `onCreate() activity lifecycle test`() {
+        verify(testActivity.viewModel, atLeastOnce()).attachView()
+    }
+
+    @Test
+    fun `onStart() activity lifecycle test`() {
+        testActivityController.start()
+        verify(testActivity.viewModel, atLeastOnce()).onStart()
+    }
+
+    @Test
+    fun `onPause() activity lifecycle test`() {
+        testActivityController.resume().pause()
+        verify(testActivity.viewModel, atLeastOnce()).onPause()
+    }
+
+    @Test
+    fun `onResume() activity lifecycle test`() {
+        testActivityController.resume()
+        verify(testActivity.viewModel, atLeastOnce()).onResume()
+    }
+
+    @Test
+    fun `onStop() activity lifecycle test`() {
+        testActivityController.resume().stop()
+        verify(testActivity.viewModel, atLeastOnce()).detachView()
+    }
+
+    @Test
+    fun `onDestroy() activity lifecycle test`() {
+        testActivityController.resume().destroy()
+        verify(testActivity.viewModel, atLeastOnce()).killView()
     }
 }
--- library/src/test/java/com/link184/architecture/mvvm/testModules.kt
+++ library/src/test/java/com/link184/architecture/mvvm/testModules.kt
@@ -1,7 +1,10 @@
 package com.link184.architecture.mvvm
 
+import com.link184.architecture.mvvm.component.TestViewModel
+import com.nhaarman.mockitokotlin2.spy
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val testModule = module {
-
+    viewModel { spy(TestViewModel()) }
 }
