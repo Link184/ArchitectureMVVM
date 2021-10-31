268ad6d77fc4d9e4ff37b40f63101cab6824d936
Link184 <pacific.trac@gmail.com>
2021-10-31 11:08:24 +0200

    Connected data state lister to fragment and activity

--- android-mvvm-compose/src/main/java/com/link184/architecture/mvvm/compose/GlobalDataStateHandler.kt
+++ android-mvvm-compose/src/main/java/com/link184/architecture/mvvm/compose/GlobalDataStateHandler.kt
@@ -1,9 +1,11 @@
 package com.link184.architecture.mvvm.compose
 
 import androidx.compose.runtime.Composable
+import com.link184.architecture.mvvm.base.MvvmActivity
+import com.link184.architecture.mvvm.base.MvvmFragment
 
 @Composable
-fun GlobalDataStateHandler(
+fun MvvmActivity<*>.GlobalDataStateHandler(
     successHandler: ((Any?) -> Unit)? = null,
     progressHandler: (() -> Unit)? = null,
     exceptionHandler: ((Throwable) -> Unit)? = null,
@@ -12,5 +14,28 @@ fun GlobalDataStateHandler(
     LocalDataStateHandler.current.successHandler = successHandler
     LocalDataStateHandler.current.progressHandler = progressHandler
     LocalDataStateHandler.current.exceptionHandler = exceptionHandler
+    viewModel.handleDataStateEvents(
+        exceptionHandler = exceptionHandler,
+        progressHandler = progressHandler,
+        successHandler = successHandler
+    )
+    content()
+}
+
+@Composable
+fun MvvmFragment<*>.GlobalDataStateHandler(
+    successHandler: ((Any?) -> Unit)? = null,
+    progressHandler: (() -> Unit)? = null,
+    exceptionHandler: ((Throwable) -> Unit)? = null,
+    content: @Composable () -> Unit
+) {
+    LocalDataStateHandler.current.successHandler = successHandler
+    LocalDataStateHandler.current.progressHandler = progressHandler
+    LocalDataStateHandler.current.exceptionHandler = exceptionHandler
+    viewModel.handleDataStateEvents(
+        exceptionHandler = exceptionHandler,
+        progressHandler = progressHandler,
+        successHandler = successHandler
+    )
     content()
 }
